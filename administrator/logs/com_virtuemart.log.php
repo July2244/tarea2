6434c39bc2e0c495dc875abc0b1aca7341f5e988
July2244 <1530550@upv.edu.mx>
2019-02-05 14:56:34 -0600

    tarea2

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,44 @@
+#
+#<?php die("Forbidden."); ?>
+
+2019-02-03 01:48:22 ERROR vmError: Warning, the Safe Path is &lt;b&gt;not accessible&lt;/b&gt; (does not exist or no permission). Use this link to the &lt;a href='http://localhost:88/feradoTienda/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2019-02-03 01:48:26 ERROR vmError: Warning, the Safe Path is &lt;b&gt;not accessible&lt;/b&gt; (does not exist or no permission). Use this link to the &lt;a href='http://localhost:88/feradoTienda/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2019-02-03 01:48:36 ERROR vmError: COM_VIRTUEMART_CANNOT_STORE_CONFIG
+2019-02-03 02:02:26 ERROR vmError: Media files must have a title
+2019-02-03 02:02:26 ERROR vmError: Sorry,It's not a valid media file
+2019-02-03 02:03:48 ERROR vmError: Media files must have a title
+2019-02-03 02:03:48 ERROR vmError: Sorry,It's not a valid media file
+2019-02-03 02:09:56 ERROR vmError: Media files must have a title
+2019-02-03 02:09:56 ERROR vmError: Sorry,It's not a valid media file
+2019-02-03 02:12:45 ERROR vmError: Media files must have a title
+2019-02-03 02:12:45 ERROR vmError: Sorry,It's not a valid media file
+2019-02-03 02:14:13 ERROR vmError: Media files must have a title
+2019-02-03 02:14:13 ERROR vmError: Sorry,It's not a valid media file
+2019-02-03 04:11:00 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/dress-1.15.jpg
+2019-02-03 04:11:00 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_12.jpg
+2019-02-03 04:11:00 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_126.jpg
+2019-02-03 04:11:00 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_09.jpg
+2019-02-03 04:11:00 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_104.jpg
+2019-02-03 04:11:00 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_11.jpg
+2019-02-03 04:11:02 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_03.jpg
+2019-02-03 04:11:02 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_08.jpg
+2019-02-03 04:11:07 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_09 copy.jpg
+2019-02-03 04:11:07 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_09 copy 2.jpg
+2019-02-03 04:11:07 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_09 copy 3.jpg
+2019-02-03 04:11:07 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/boy-51.jpg
+2019-02-03 04:11:07 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/boy-518.jpg
+2019-02-03 04:12:41 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/dress-1.15.jpg
+2019-02-03 04:12:41 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_12.jpg
+2019-02-03 04:12:41 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_126.jpg
+2019-02-03 04:12:41 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_09.jpg
+2019-02-03 04:12:41 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_104.jpg
+2019-02-03 04:12:41 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_11.jpg
+2019-02-03 04:12:48 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_03.jpg
+2019-02-03 04:12:48 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_08.jpg
+2019-02-03 04:12:51 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/dress-1.15.jpg
+2019-02-03 04:12:51 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_12.jpg
+2019-02-03 04:12:51 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_126.jpg
+2019-02-03 04:12:51 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_09.jpg
+2019-02-03 04:12:51 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_104.jpg
+2019-02-03 04:12:51 ERROR vmError: Couldnt create thumb, file not found /opt/lampp/htdocs/feradoTienda/images/joomlashine/sample/stories/virtuemart/product/product_11.jpg
+2019-02-03 06:35:36 ERROR vmError: Media files must have a title
